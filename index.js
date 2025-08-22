58bcffd2194626beada90bcbc0ec69b082794c62
nimeralmashni <nsoccer2009@gmail.com>
2025-08-22 08:56:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log ("nimer")
+ let firstName = "nimer"
+ let lastName = "almashni"
+ let name = ["hello", "nimer"].join(" ")
+ console.log(name)
+ console.log(`hello ${firstName} ${lastName}`);
