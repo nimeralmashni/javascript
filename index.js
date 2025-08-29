6060578f99f12024ad72bda094b9a6eed9a3b57b
nimeralmashni <nsoccer2009@gmail.com>
2025-08-29 08:28:59 -0500

    mus

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log ("nimer")
- let firstName = "nimer"
- let lastName = "almashni"
- let name = ["hello", "nimer"].join(" ")
- console.log(name)
- console.log(`hello ${firstName} ${lastName}`);
--- null-undefined.js
+++
@@ -1,8 +0,0 @@
-//what happens when you dont give a variable a value
-let x;
-console.log(x);
-console.log(typeof x)
-x=5;
-console.log("stores a 5:",typeof x)
-x=null;
-console.log("stores a null:",typeof x)
