4f168053b65ddfea49d80be06e9982e4b4b289df
nimeralmashni <nsoccer2009@gmail.com>
2025-08-29 07:54:50 -0500

    hiiii

---
+++ null-undefined.js
@@ -0,0 +1,8 @@
+//what happens when you dont give a variable a value
+let x;
+console.log(x);
+console.log(typeof x)
+x=5;
+console.log("stores a 5:",typeof x)
+x=null;
+console.log("stores a null:",typeof x)
---
+++ strings.js
@@ -0,0 +1,12 @@
+//primative wrapper methods//
+let firstName= "how old are you david"
+console.log (typeof firstName);
+console.log (firstName.charAt(4));
+console.log(firstName.concat(['value1','value2']));
+console.log(firstName.length)
+console.log(firstName.endsWith('you'))
+console.log(firstName.indexOf('nimer'))
+console.log(firstName.lastIndexOf('a'))
+
+git.config global user.email "nsoccer2009@gmail.com"
+git.config global user.name "nimeralmashni"
